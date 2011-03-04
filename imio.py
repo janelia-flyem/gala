7c3068267c3a1d4094881b58dce1e787f6685953
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-03-03 23:57:43 -0500

    change default compression and default chunking to None for write_h5_stack, because Matlab doesn't support either. Add read_h5_stack. Add support for file list arguments to read_image_stack.

--- imio.py
+++ imio.py
@@ -5,13 +5,16 @@ import h5py, Image, numpy
 
 from fnmatch import filter as fnfilter
 from os.path import split as split_path, join as join_path
-from numpy import asarray, uint8, uint16, uint32, zeros
+from numpy import array, asarray, uint8, uint16, uint32, zeros
 
-def read_image_stack(fn, *args):
+def read_image_stack(fn, *args, **kwargs):
     d, fn = split_path(fn)
     if len(d) == 0: d = '.'
     if fn.endswith('.png'):
-        fns = fnfilter(os.listdir(d), fn)
+        if len(args) > 0 and type(args[0]) == str and args[0].endswith('png'):
+            fns = [fn] + [split_path(f)[1] for f in args]
+        else:
+            fns = fnfilter(os.listdir(d), fn)
         ims = (Image.open(join_path(d,fn)) for fn in sorted(fns))
         ars = (asarray(im) for im in ims)
         w, h = Image.open(join_path(d,fns[0])).size
@@ -19,7 +22,19 @@ def read_image_stack(fn, *args):
         stack = zeros([h,w,len(fns)], dtype)
         for i, im in enumerate(ars):
             stack[:,:,i] = im
+    if fn.endswith('.h5'):
+        stack = read_h5_stack('/'.join([d,fn]), *args, **kwargs)
     return stack
+
+def read_h5_stack(fn, *args, **kwargs):
+    if len(args) > 0:
+        group = args[0]
+    elif kwargs.has_key('group'):
+        group = kwargs['group']
+    else:
+        group = 'stack'
+    dset = h5py.File(fn, 'r')
+    return array(dset[group])
 
 def write_image_stack(npy_vol, fn, **kwargs):
     fn = os.path.expanduser(fn)
@@ -45,7 +60,9 @@ def write_png_image_stack(npy_vol, fn, **kwargs):
 
 def write_h5_stack(npy_vol, fn, **kwargs):
     if not kwargs.has_key('compression'):
-        kwargs['compression'] = 'lzf'
+        kwargs['compression'] = None
+    if not kwargs.has_key('chunks'):
+        kwargs['chunks'] = None
     try:
         group = kwargs['group']
     except KeyError:
