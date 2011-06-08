67375e08c5094d1be2db34b2355c9eaa4866c74a
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-06-08 18:03:32 -0400

    Fixed non-PEP whitespace in Ryan's commit

--- eval.py
+++ eval.py
@@ -20,52 +20,52 @@ def edit_distance_to_bps(aseg, bps):
     return (false_merges, false_splits)
 
 def body_rand(seg, gt):
-	"""Calculates the rand index of a segmentation versus ground truth.
-		Returns the rand index, adjusted rand index, and Fowlkes-Mallows index
-		
-		Equations based on the paper 
-			"On the Use of the Adjusted Rand Index as a Metric for 
-			Evaluating Supervised Classication", Santos and Embrechts
-	"""
-	# Initialize variables
-	tp = 0
-	tn = 0
-	fp = 0
-	fn = 0
-	gtr = gt.ravel()
-	segr = seg.ravel()
-	
-	# Re-number the segmentations in so they have sequentially-numbered segments
-	ctr = 0
-	for i in numpy.unique(gtr):
-		gtr[numpy.nonzero(gtr==i)] = ctr
-		ctr = ctr + 1
-	ctr = 0
-	for i in numpy.unique(segr):
-		segr[numpy.nonzero(segr==i)] = ctr
-		ctr = ctr + 1
-	
-	# Get contingency table
-	cont = numpy.zeros((numpy.max(gtr)+1, numpy.max(segr)+1))
-	for i in range(numpy.size(gt)):
-		cont[gtr[i], segr[i]] = cont[gtr[i], segr[i]] + 1
-		
-	# Calculate values
-	n = numpy.sum(cont)
-	a = (numpy.sum(numpy.power(cont,2)) - n)/2.0;
-	b = (numpy.sum(numpy.power(numpy.sum(cont,1),2)) - 
-		numpy.sum(numpy.power(cont,2)))/2
-	c = (numpy.sum(numpy.power(numpy.sum(cont,0),2)) - 
-		numpy.sum(numpy.power(cont,2)))/2
-	d = (numpy.sum(numpy.power(cont,2)) + n**2 - 
-		numpy.sum(numpy.power(numpy.sum(cont,1),2)) - 
-		numpy.sum(numpy.power(numpy.sum(cont,0),2)))/2
-		
-	# Get indices
-	RI = (a+d)/(a+b+c+d)
-	ARI = (nchoosek(n,2)*(a+d) - ((a+b)*(a+c) + (c+d)*(b+d)))/(
-		nchoosek(n,2)**2 - ((a+b)*(a+c) + (c+d)*(b+d)))
-	FM = a/(numpy.sqrt((a+b)*(a+c)))
-		
-	return (RI, ARI, FM)
+    """Calculates the rand index of a segmentation versus ground truth.
+        Returns the rand index, adjusted rand index, and Fowlkes-Mallows index
+        
+        Equations based on the paper 
+            "On the Use of the Adjusted Rand Index as a Metric for 
+            Evaluating Supervised Classication", Santos and Embrechts
+    """
+    # Initialize variables
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    gtr = gt.ravel()
+    segr = seg.ravel()
+    
+    # Re-number the segmentations in so they have sequentially-numbered segments
+    ctr = 0
+    for i in numpy.unique(gtr):
+        gtr[numpy.nonzero(gtr==i)] = ctr
+        ctr = ctr + 1
+    ctr = 0
+    for i in numpy.unique(segr):
+        segr[numpy.nonzero(segr==i)] = ctr
+        ctr = ctr + 1
+    
+    # Get contingency table
+    cont = numpy.zeros((numpy.max(gtr)+1, numpy.max(segr)+1))
+    for i in range(numpy.size(gt)):
+        cont[gtr[i], segr[i]] = cont[gtr[i], segr[i]] + 1
+        
+    # Calculate values
+    n = numpy.sum(cont)
+    a = (numpy.sum(numpy.power(cont,2)) - n)/2.0;
+    b = (numpy.sum(numpy.power(numpy.sum(cont,1),2)) - 
+        numpy.sum(numpy.power(cont,2)))/2
+    c = (numpy.sum(numpy.power(numpy.sum(cont,0),2)) - 
+        numpy.sum(numpy.power(cont,2)))/2
+    d = (numpy.sum(numpy.power(cont,2)) + n**2 - 
+        numpy.sum(numpy.power(numpy.sum(cont,1),2)) - 
+        numpy.sum(numpy.power(numpy.sum(cont,0),2)))/2
+        
+    # Get indices
+    RI = (a+d)/(a+b+c+d)
+    ARI = (nchoosek(n,2)*(a+d) - ((a+b)*(a+c) + (c+d)*(b+d)))/(
+        nchoosek(n,2)**2 - ((a+b)*(a+c) + (c+d)*(b+d)))
+    FM = a/(numpy.sqrt((a+b)*(a+c)))
+        
+    return (RI, ARI, FM)
     
