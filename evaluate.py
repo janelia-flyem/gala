19f7e39940b7b1b45d96685acc4c6a8456e959cb
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-06-15 17:54:02 -0400

    Small formatting fixes. Renamed eval to evaluate to prevent conflict with built-in eval()

--- evaluate.py
+++ evaluate.py
@@ -23,54 +23,53 @@ def edit_distance_to_bps(aseg, bps):
 
 
 def contingency_table(seg, gt):
-    """ Return the contingency table for all pairs of regions of the two segmentations. """
+    """Return the contingency table for all regions in matched segmentations."""
     # Re-number the segmentations in so they have sequentially-numbered segments
     sel = numpy.zeros((3,3))
     sel[:,1] = 1
     sel[1,:] = 1
-    gt,n =label(gt, sel)
+    gt,n = label(gt, sel)
     seg,n = label(seg, sel)
-    
+
     gtr = numpy.ravel(gt)
     segr = numpy.ravel(seg)
-    
+
     # Get contingency table
     cont = numpy.zeros((numpy.max(gtr)+1, numpy.max(segr)+1))
     for i in range(numpy.size(gt)):
         cont[gtr[i], segr[i]] = cont[gtr[i], segr[i]] + 1
-        
+
     return cont
     
 def voi(X, Y, cont=None):
-    """ Return the variation of information metric. """
+    """Return the variation of information metric."""
     return numpy.sum(split_voi(X,Y,cont))
 
 def split_voi(X, Y, cont=None):
-    """ Return the conditional entropies associated with the variation of information.
+    """Return the symmetric conditional entropies associated with the VOI.
     
     The variation of information is defined as VI(X,Y) = H(X|Y) + H(Y|X).
     If Y is the ground-truth segmentation, then H(Y|X) can be interpreted
     as the amount of under-segmentation of Y and H(X|Y) is then the amount
     of over-segmentation.  In other words, a perfect over-segmentation
     will have H(Y|X)=0 and a perfect under-segmentation will have H(X|Y)=0.
-
     """
     if cont is None:
         cont = contingency_table(X, Y)
-    
+
     n = numpy.sum(cont)
     s1,s2 = numpy.shape(cont)
-    
+
     # Calculate probabilities
     pxy = cont/float(n)
     px = numpy.sum(pxy,0)
     py = numpy.sum(pxy,1)
-    
+
     # Calculate log conditional probabilities
     lpygx = numpy.divide(pxy, numpy.tile(px, (s1,1))) # log P(Y|X)
     r,c = numpy.nonzero(pxy)
     lpygx[r,c] = numpy.log2(lpygx[r,c])
-    
+
     lpxgy = numpy.divide(pxy, numpy.tile(py, (s2,1)).transpose()) # log P(X|Y)
     r,c = numpy.nonzero(pxy)
     lpxgy[r,c] = numpy.log2(lpxgy[r,c])
@@ -84,7 +83,7 @@ def rand_index(seg, gt, cont=None):
     """ Return the unadjusted Rand index. """
     if cont is None:
         cont = contingency_table(seg, gt)
-    
+
     # Calculate values for rand indices
     n = numpy.sum(cont)
     sum1 = numpy.sum(numpy.power(cont,2))
@@ -94,14 +93,14 @@ def rand_index(seg, gt, cont=None):
     b = (sum2 - sum1)/2
     c = (sum3 - sum1)/2
     d = (sum1 + n**2 - sum2 - sum3)/2
-        
+
     return (a+d)/(a+b+c+d)
     
 def adj_rand_index(seg, gt, cont=None):
     """ Return the adjusted Rand index. """
     if cont is None:
         cont = contingency_table(seg, gt)
-    
+
     # Calculate values for rand indices
     n = numpy.sum(cont)
     sum1 = numpy.sum(numpy.power(cont,2))
@@ -111,7 +110,7 @@ def adj_rand_index(seg, gt, cont=None):
     b = (sum2 - sum1)/2
     c = (sum3 - sum1)/2
     d = (sum1 + n**2 - sum2 - sum3)/2
-        
+
     return (nchoosek(n,2)*(a+d) - ((a+b)*(a+c) + (c+d)*(b+d)))/(
         nchoosek(n,2)**2 - ((a+b)*(a+c) + (c+d)*(b+d)))
         
@@ -119,7 +118,7 @@ def fm_index(seg, gt, cont=None):
     """ Return the Fowlkes-Mallows index. """
     if cont is None:
         cont = contingency_table(seg, gt)
-    
+
     # Calculate values for rand indices
     n = numpy.sum(cont)
     sum1 = numpy.sum(numpy.power(cont,2))
@@ -129,6 +128,6 @@ def fm_index(seg, gt, cont=None):
     b = (sum2 - sum1)/2
     c = (sum3 - sum1)/2
     d = (sum1 + n**2 - sum2 - sum3)/2
-        
+
     return a/(numpy.sqrt((a+b)*(a+c)))
-    
+
