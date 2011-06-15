b6dc243da5ffa22ce4a6ee2d797dcd23e02a907e
Ryan <kennedyr@janelia.hhmi.org>
2011-06-15 15:44:26 -0400

    split up eval_seg() function
    Signed-off-by: Juan Nunez-Iglesias <jni@janelia.hhmi.org>

--- eval.py
+++ eval.py
@@ -1,5 +1,6 @@
 import numpy
 import agglo
+from scipy.ndimage.measurements import label
 from scipy.misc import comb as nchoosek
 
 def pixel_wise_boundary_precision_recall(aseg, gt):
@@ -19,58 +20,34 @@ def edit_distance_to_bps(aseg, bps):
     false_splits = (r.overlaps.sum(axis=0)-1)[1:].sum()
     false_merges = (r.overlaps.sum(axis=1)-1)[1:].sum()
     return (false_merges, false_splits)
-        
 
-def eval_seg(seg, gt):
-    """Evaluates a segmentation against the ground truth.
-        Returns the rand index, adjusted rand index, Fowlkes-Mallows index,
-            and variation of information.
-        
-        Equations for RI, ARI and FM are based on the paper 
-            "On the Use of the Adjusted Rand Index as a Metric for 
-            Evaluating Supervised Classication", Santos and Embrechts
-    """
-    # Initialize variables
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
-    gtr = gt.ravel()
-    segr = seg.ravel()
-    
+
+def contingency_table(seg, gt):
+    """ Return the contingency table for all pairs of regions of the two segmentations. """
     # Re-number the segmentations in so they have sequentially-numbered segments
-    ctr = 0
-    for i in numpy.unique(gtr):
-        gtr[numpy.nonzero(gtr==i)] = ctr
-        ctr = ctr + 1
-    ctr = 0
-    for i in numpy.unique(segr):
-        segr[numpy.nonzero(segr==i)] = ctr
-        ctr = ctr + 1
+    sel = numpy.zeros((3,3))
+    sel[:,1] = 1
+    sel[1,:] = 1
+    gt,n =label(gt, sel)
+    seg,n = label(seg, sel)
+    
+    gtr = numpy.ravel(gt)
+    segr = numpy.ravel(seg)
     
     # Get contingency table
     cont = numpy.zeros((numpy.max(gtr)+1, numpy.max(segr)+1))
     for i in range(numpy.size(gt)):
         cont[gtr[i], segr[i]] = cont[gtr[i], segr[i]] + 1
         
-    # Calculate values for rand indices
-    n = numpy.sum(cont)
-    a = (numpy.sum(numpy.power(cont,2)) - n)/2.0;
-    b = (numpy.sum(numpy.power(numpy.sum(cont,1),2)) - 
-        numpy.sum(numpy.power(cont,2)))/2
-    c = (numpy.sum(numpy.power(numpy.sum(cont,0),2)) - 
-        numpy.sum(numpy.power(cont,2)))/2
-    d = (numpy.sum(numpy.power(cont,2)) + n**2 - 
-        numpy.sum(numpy.power(numpy.sum(cont,1),2)) - 
-        numpy.sum(numpy.power(numpy.sum(cont,0),2)))/2
-        
-    # Get rand indices
-    RI = (a+d)/(a+b+c+d)
-    ARI = (nchoosek(n,2)*(a+d) - ((a+b)*(a+c) + (c+d)*(b+d)))/(
-        nchoosek(n,2)**2 - ((a+b)*(a+c) + (c+d)*(b+d)))
-    FM = a/(numpy.sqrt((a+b)*(a+c)))
+    return cont
     
-    # Calculate values for Variation of Information
+def voi(seg, gt, cont=None):
+    """ Return the variation of information metric. """
+    if cont is None:
+        cont = contingency_table(seg, gt)
+    
+    n = numpy.sum(cont)
+    # Calculate values for variation of information
     s1,s2 = numpy.shape(cont)
     pxy = cont/float(n) #P(X,Y)
     px = numpy.sum(pxy,0)
@@ -82,10 +59,60 @@ def eval_seg(seg, gt):
     r,c = numpy.nonzero(pxy)
     lpxgy[r,c] = numpy.log2(lpxgy[r,c])
 
-    # Get Variation of information
+    # Get variation of information
     hygx = -numpy.sum(numpy.multiply(pxy, lpygx))
     hxgy = -numpy.sum(numpy.multiply(pxy, lpxgy))
-    VOI = hygx + hxgy
+    return hygx + hxgy
+
+def rand_index(seg, gt, cont=None):
+    """ Return the unadjusted Rand index. """
+    if cont is None:
+        cont = contingency_table(seg, gt)
+    
+    # Calculate values for rand indices
+    n = numpy.sum(cont)
+    sum1 = numpy.sum(numpy.power(cont,2))
+    sum2 = numpy.sum(numpy.power(numpy.sum(cont,1),2))
+    sum3 = numpy.sum(numpy.power(numpy.sum(cont,0),2))
+    a = (sum1 - n)/2.0;
+    b = (sum2 - sum1)/2
+    c = (sum3 - sum1)/2
+    d = (sum1 + n**2 - sum2 - sum3)/2
         
-    return (RI, ARI, FM, VOI)
+    return (a+d)/(a+b+c+d)
+    
+def adj_rand_index(seg, gt, cont=None):
+    """ Return the adjusted Rand index. """
+    if cont is None:
+        cont = contingency_table(seg, gt)
+    
+    # Calculate values for rand indices
+    n = numpy.sum(cont)
+    sum1 = numpy.sum(numpy.power(cont,2))
+    sum2 = numpy.sum(numpy.power(numpy.sum(cont,1),2))
+    sum3 = numpy.sum(numpy.power(numpy.sum(cont,0),2))
+    a = (sum1 - n)/2.0;
+    b = (sum2 - sum1)/2
+    c = (sum3 - sum1)/2
+    d = (sum1 + n**2 - sum2 - sum3)/2
+        
+    return (nchoosek(n,2)*(a+d) - ((a+b)*(a+c) + (c+d)*(b+d)))/(
+        nchoosek(n,2)**2 - ((a+b)*(a+c) + (c+d)*(b+d)))
+        
+def fm_index(seg, gt, cont=None):
+    """ Return the Fowlkes-Mallows index. """
+    if cont is None:
+        cont = contingency_table(seg, gt)
+    
+    # Calculate values for rand indices
+    n = numpy.sum(cont)
+    sum1 = numpy.sum(numpy.power(cont,2))
+    sum2 = numpy.sum(numpy.power(numpy.sum(cont,1),2))
+    sum3 = numpy.sum(numpy.power(numpy.sum(cont,0),2))
+    a = (sum1 - n)/2.0;
+    b = (sum2 - sum1)/2
+    c = (sum3 - sum1)/2
+    d = (sum1 + n**2 - sum2 - sum3)/2
+        
+    return a/(numpy.sqrt((a+b)*(a+c)))
     
