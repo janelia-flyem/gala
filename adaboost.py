c97800e42d18f2463a54bf2bbca94ebd57b5d56b
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-07-28 14:08:41 -0400

    some adaboost cleanup. Made vigra import optional (warning only if import fails).

--- adaboost.py
+++ adaboost.py
@@ -1,12 +1,21 @@
-
-from decision_stump import DecisionStump
-
-import sys, math, random
-import numpy
+# system modules
+import sys, math, random, logging
 import operator
 
+# libraries
+import numpy
+
+# local modules
+from decision_stump import DecisionStump
+from iterprogress import with_progress, NoProgressBar, StandardProgressBar
+
 class AdaBoost(object):
-    """ Class for an adaboost classifier, adapted from pyclassic. """
+    """Class for an adaboost classifier, adapted from pyclassic. """
+    def __init__(self, progress=False, **kwargs):
+        if progress:
+            self.progressbar = StandardProgressBar('AdaBoost training...')
+        else:
+            self.progressbar = NoProgressBar()
 
     def fit(self, X, Y, w=None, w_asymmetric=None, T=100):
         self.X = X.copy()
@@ -15,43 +24,42 @@ class AdaBoost(object):
         
         if w is None:
             w = (1.0/float(N))*numpy.ones(N)
-	if w_asymmetric is None:
-	    w_asymmetric = (1.0/float(N))*numpy.ones(N)
+        if w_asymmetric is None:
+            w_asymmetric = (1.0/float(N))*numpy.ones(N)
         self.weights = w.copy()
-	self.weights_asymmetric = numpy.array([i**(1.0/float(T)) for i in w_asymmetric])
-	self.weights /= float(sum(self.weights))
+        self.weights_asymmetric = numpy.array([i**(1.0/float(T)) 
+                                                        for i in w_asymmetric])
+        self.weights /= float(sum(self.weights))
         self.weak_classifier_ensemble = []
         self.alpha = []
         
-        for t in range(T):
-	    # Apply asymmetric weights
-	    self.weights *= self.weights_asymmetric
+        for t in with_progress(range(T), pbar=self.progressbar):
+            # Apply asymmetric weights
+            self.weights *= self.weights_asymmetric
             weak_learner = DecisionStump().fit(self.X,self.Y,self.weights)
             Y_pred = weak_learner.predict(self.X)
             e = sum(0.5*self.weights*abs(self.Y-Y_pred))/sum(self.weights)
-	    if e > 0.5:
-		sys.stdout.write('WARNING: ending training due to no good weak classifiers.')
+            if e > 0.5:
+                logging.warning(' ending training, no good weak classifiers.')
                 break
             ee = (1.0-e)/float(e)
             alpha = 0.5*math.log(ee)
-            self.weights *= numpy.exp(-alpha*self.Y*Y_pred) # increase weights for wrongly classified
+            # increase weights for wrongly classified:
+            self.weights *= numpy.exp(-alpha*self.Y*Y_pred)
             self.weights /= sum(self.weights)
             self.weak_classifier_ensemble.append(weak_learner)
             self.alpha.append(alpha)
-            self.T = t+1
-            sys.stdout.write('.')
-            sys.stdout.flush()
-        print "\n"
         return self
 
-    def predict(self,X):
+    def predict_score(self,X):
         X = numpy.array(X)
-        Y = sum([self.alpha[i]*self.weak_classifier_ensemble[i].predict(X) for i in xrange(len(self.alpha))])
+        Y = sum([alpha * weak_classifier.predict(X) for alpha, weak_classifier
+                            in zip(self.alpha, self.weak_classifier_ensemble)])
         return Y
         
     def predict_proba(self, X):
-        prob = 1.0/(1.0 + numpy.exp(-self.predict(X)))
-        return numpy.concatenate((numpy.array([1.0-prob]), numpy.array([prob]))).T
+        p = 1.0/(1.0 + 2*numpy.exp(-self.predict_score(X)))
+        return numpy.array([1-p, p])
         
 
 def measure_accuracy(Y, o, threshold=0):
