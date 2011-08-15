638b9762915a423a55f01247ffc370a3fe1a5b79
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-08-15 15:40:41 -0400

    Allowed import/running of ray in the absence of iterprogress. No progress will be shown but other functionality will not be affected.

--- iterprogress.py
+++ iterprogress.py
@@ -1,4 +1,4 @@
-from progressbar import ProgressBar, Percentage, Bar, ETA, RotatingMarker
+import logging
 
 class NoProgressBar(object):
     def __init__(self, *args, **kwargs): pass
@@ -7,41 +7,6 @@ class NoProgressBar(object):
     def update_i(self, *args, **kwargs): pass
     def finish(self, *args, **kwargs): pass
     def set_title(self, *args, **kwargs): pass
-
-class StandardProgressBar(object):
-    def __init__(self, title='Progress: '):
-        self.title = title
-        self.is_finished = False
-
-    def start(self, total, widgets=None):
-        if widgets is None:
-            widgets = [self.title, RotatingMarker(), ' ',
-                            Percentage(), ' ', Bar(marker='='), ' ', ETA()]
-        self.pbar = ProgressBar(widgets=widgets, maxval=total)
-        self.pbar.start()
-        self.i = 0
-
-    def update(self, step=1):
-        self.i += step
-        self.pbar.update(self.i)
-        if self.i == self.pbar.maxval:
-            self.finish()
-
-    def update_i(self, value):
-        self.i = value
-        self.pbar.update(value)
-        if value == self.pbar.maxval:
-            self.finish()
-
-    def finish(self):
-        if self.is_finished:
-            pass
-        else:
-            self.pbar.finish()
-            self.is_finished = True
-
-    def set_title(self, title):
-        self.title = title
 
 def with_progress(collection, length=None, title=None, pbar=NoProgressBar()):
     if length is None:
@@ -52,4 +17,47 @@ def with_progress(collection, length=None, title=None, pbar=NoProgressBar()):
     for elem in collection:
         yield elem
         pbar.update()
+
+try:
+    from progressbar import ProgressBar, Percentage, Bar, ETA, RotatingMarker
+except ImportError:
+    logging.warning(' progressbar package not installed. Progress cannot be '+
+        'shown. See http://pypi.python.org/simple/progressbar or type '+
+        '"sudo easy_install progressbar" to fix.')
+    StandardProgressBar = NoProgressBar
+else:
+    class StandardProgressBar(object):
+        def __init__(self, title='Progress: '):
+            self.title = title
+            self.is_finished = False
+
+        def start(self, total, widgets=None):
+            if widgets is None:
+                widgets = [self.title, RotatingMarker(), ' ',
+                            Percentage(), ' ', Bar(marker='='), ' ', ETA()]
+            self.pbar = ProgressBar(widgets=widgets, maxval=total)
+            self.pbar.start()
+            self.i = 0
+
+        def update(self, step=1):
+            self.i += step
+            self.pbar.update(self.i)
+            if self.i == self.pbar.maxval:
+                self.finish()
+
+        def update_i(self, value):
+            self.i = value
+            self.pbar.update(value)
+            if value == self.pbar.maxval:
+                self.finish()
+
+        def finish(self):
+            if self.is_finished:
+                pass
+            else:
+                self.pbar.finish()
+                self.is_finished = True
+
+        def set_title(self, title):
+            self.title = title
 
