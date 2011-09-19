ad5649f1d61f230bbb3ff7ced8b39591ed67c016
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-09-19 14:52:55 -0400

    Add new plot types to viz.py. Finetune annotefinder.

--- ray/viz.py
+++ ray/viz.py
@@ -5,6 +5,7 @@ import scipy
 import evaluate
 import matplotlib
 plt = matplotlib.pyplot
+from itertools import cycle
 
 label=scipy.ndimage.measurements.label
 
@@ -86,9 +87,9 @@ def plot_voi_breakdown_panel(px, h, title, xlab, ylab, hlines, **kwargs):
     x = scipy.arange(max(min(px),1e-10), max(px), (max(px)-min(px))/100.0)
     for val in hlines:
         plt.plot(x, val/x, 'g:') 
-    plt.scatter(px, h, c=kwargs['c'], label=title)
+    plt.scatter(px, h, label=title, **kwargs)
     af = AnnoteFinder(px, h, [str(i) for i in range(len(px))], 
-        xtol=10, ytol=10, xmin=-0.05*max(px), ymin=-0.05*max(px), 
+        xtol=0.005, ytol=0.005, xmin=-0.05*max(px), ymin=-0.05*max(px), 
         xmax = 1.05*max(px), ymax=1.05*max(h))
     plt.connect('button_press_event', af)
     plt.xlim(xmin=-0.05*max(px), xmax=1.05*max(px))
@@ -98,7 +99,7 @@ def plot_voi_breakdown_panel(px, h, title, xlab, ylab, hlines, **kwargs):
     plt.title(title)
 
 def plot_voi_breakdown(seg, gt, ignore_seg=[], ignore_gt=[], 
-                                                    hlines=None, subplot=False):
+                                        hlines=None, subplot=False, **kwargs):
     """Plot conditional entropy H(Y|X) vs P(X) for both seg|gt and gt|seg."""
     plt.ion()
     pxy,px,py,hxgy,hygx,lpygx,lpxgy = evaluate.voi_tables(seg,gt,
@@ -117,41 +118,66 @@ def plot_voi_breakdown(seg, gt, ignore_seg=[], ignore_gt=[],
     if subplot: plt.subplot(1,2,1)
     plot_voi_breakdown_panel(px, -lpygx, 
         'Undersegmentation', 'p(S=seg)', 'H(G|S=seg)', 
-        hlines, c='r')
+        hlines, c='r', **kwargs)
     if subplot: plt.subplot(1,2,2)
     plot_voi_breakdown_panel(py, -lpxgy, 
         'Oversegmentation', 'p(G=gt)', 'H(S|G=gt)', 
-        hlines, c='b')
+        hlines, c='b', **kwargs)
     if not subplot:
         plt.title('VOI contributions by body.')
-        plt.legend(loc='lower right')
+        plt.legend(loc='lower right', scatterpoints=1)
         plt.xlabel('$p_x$')
         plt.ylabel('H(Y|X=x)')
+        xmax = max(px.max(), py.max())
+        plt.xlim(-0.05*xmax, 1.05*xmax)
+        ymax = max(-lpygx.min(), -lpxgy.min())
+        plt.ylim(-0.05*ymax, 1.05*ymax)
 
 def plot_voi_parts(*args, **kwargs):
     kwargs['subplot'] = True
     plot_voi_breakdown(*args, **kwargs)
 
-def plot_split_voi(ucms, gt, ignore_seg=[], ignore_gt=[], 
-                                                linespecs=['k-'], npoints=100):
-    if type(ucms) not in [list, tuple]:
-        ucms = [ucms]
-    if type(linespecs) not in [list, tuple]:
-        linespecs = [linespecs]
-    if len(linespecs) < len(ucms):
-        linespecs = cycle(linespecs)
-    results = []
-    for ucm, linespec in zip(ucms, linespecs):
-        ts = np.unique(ucm)[1:]
-        if len(ts) > 2*npoints:
-            ts = ts[np.arange(1, len(ts), len(ts)/npoints)]
-        result = np.zeros((2,len(ts)))
-        for i, t in enumerate(ts):
-            seg = label(ucm<t)[0]
-            result[:,i] = evaluate.split_voi(seg, gt, None, 
-                                                        ignore_seg, ignore_gt)
-        results.append(
-            (plt.plot(result[0], result[1], linespec),
-            np.concatenate((ts[np.newaxis, :], result), 0))
+def add_opts_to_plot(ars, colors='k', markers='^', **kwargs):
+    if type(colors) not in [list, tuple]:
+        colors = [colors]
+    if len(colors) < len(ars):
+        colors = cycle(colors)
+    if type(markers) not in [list, tuple]:
+        markers = [markers]
+    if len(markers) < len(ars):
+        markers = cycle(markers)
+    points = []
+    for ar, c, m in zip(ars, colors, markers):
+        opt = ar[:,ar.sum(axis=0).argmin()]
+        points.append(plt.scatter(opt[0], opt[1], c=c, marker=m, **kwargs))
+    return points
+
+def add_nats_to_plot(ars, tss, stops=0.5, colors='k', markers='o', **kwargs):
+    if type(colors) not in [list, tuple]: colors = [colors]
+    if len(colors) < len(ars): colors = cycle(colors)
+    if type(markers) not in [list, tuple]: markers = [markers]
+    if len(markers) < len(ars): markers = cycle(markers)
+    if type(stops) not in [list, tuple]: stops = [stops]
+    if len(stops) < len(ars): stops = cycle(stops)
+    points = []
+    for ar, ts, stop, c, m in zip(ars, tss, stops, colors, markers):
+        nat = ar[:,np.flatnonzero(ts<stop)[-1]]
+        points.append(plt.scatter(nat[0], nat[1], c=c, marker=m, **kwargs))
+    return points
+
+def plot_split_voi(ars, best=None, colors='k', linespecs='-', 
+                                        addopt=None, addnat=None, **kwargs):
+    if type(ars) not in [list, tuple]: ars = [ars]
+    if type(colors) not in [list, tuple]: colors = [colors]
+    if len(colors) < len(ars): colors = cycle(colors)
+    if type(linespecs) not in [list, tuple]: linespecs = [linespecs]
+    if len(linespecs) < len(ars): linespecs = cycle(linespecs)
+    lines = []
+    for ar, color, linespec in zip(ars, colors, linespecs):
+        lines.append(plt.plot(ar[0], ar[1], c=color, ls=linespec, **kwargs))
+    if best is not None:
+        lines.append(plt.scatter(
+            best[0], best[1], 
+            c=kwargs.get('best-color', 'm'), marker=(5,1,0), **kwargs)
         )
-    return results
+    return lines
