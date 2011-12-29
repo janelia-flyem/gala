7cdf2114265b57a1eadc1cbd2a4ccfbb95148a9f
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2011-12-29 16:19:52 -0500

    Change all references to VOI to VI.

--- ray/agglo.py
+++ ray/agglo.py
@@ -29,7 +29,7 @@ import morpho
 import iterprogress as ip
 from ncut import ncutW
 from mergequeue import MergeQueue
-from evaluate import contingency_table, split_voi, xlogx
+from evaluate import contingency_table, split_vi, xlogx
 from classify import NullFeatureManager, MomentsFeatureManager, \
     HistogramFeatureManager, RandomForest, unique_learning_data_elements, \
     concatenate_data_elements
@@ -357,7 +357,7 @@ class Rag(Graph):
                     history.append((n1,n2))
                     scores.append(merge_priority)
                     evaluation.append(
-                        (self.number_of_nodes()-1, self.split_voi())
+                        (self.number_of_nodes()-1, self.split_vi())
                     )
         if save_history:
             return history, scores, evaluation
@@ -377,7 +377,7 @@ class Rag(Graph):
                 if save_history: 
                     history.append((n1,n2))
                     evaluation.append(
-                        (self.number_of_nodes()-1, self.split_voi())
+                        (self.number_of_nodes()-1, self.split_vi())
                     )
         if save_history:
             return history, evaluation
@@ -454,7 +454,7 @@ class Rag(Graph):
             max_numepochs = 2 if learn_flat else 1
         if priority_mode in ['random', 'mean'] and not memory:
             max_numepochs = 1
-        label_type_keys = {'assignment':0, 'voi-sign':1, 
+        label_type_keys = {'assignment':0, 'vi-sign':1, 
                                                 'rand-sign':2, 'boundary':3}
         if type(gts) != list:
             gts = [gts] # allow using single ground truth as input
@@ -524,13 +524,13 @@ class Rag(Graph):
         # Calculate weights for weighting data points
         s1, s2 = [len(self.node[n]['extent']) for n in [n1, n2]]
         weights = \
-            compute_local_voi_change(s1, s2, self.volume_size), \
+            compute_local_vi_change(s1, s2, self.volume_size), \
             compute_local_rand_change(s1, s2, self.volume_size)
         # Get the fraction of times that n1 and n2 assigned to
         # same segment in the ground truths
         cont_labels = [
             [(-1)**(a[n1,:]==a[n2,:]).all() for a in assignments],
-            [compute_true_delta_voi(ctable, n1, n2) for ctable in ctables],
+            [compute_true_delta_vi(ctable, n1, n2) for ctable in ctables],
             [-compute_true_delta_rand(ctable, n1, n2, self.volume_size) 
                                                     for ctable in ctables],
             [(self.compute_boundary_overlap_with_gt(n1,n2, ws_is_gt)>
@@ -561,9 +561,9 @@ class Rag(Graph):
         Value:
             A learning data matrix of shape 
             [n_training_examples x (n_features + 5)]. The elements after the
-            features are the label, the approximate magnitude of the VOI 
-            change, the approximate magnitude of the Rand index change, and
-            the two nodes that were sampled.
+            features are the label, the approximate magnitude of the variation
+            of information (VI) change, the approximate magnitude of the Rand
+            index (RI) change, and the two nodes that were sampled.
 
         Learning modes:
             - strict: use positive-boundary examples to learn but never
@@ -571,13 +571,13 @@ class Rag(Graph):
             - loose: merge regardless of label
         Labeling modes:
             - assignment: assign each node to a gold standard node and 
-            - voi-sign: compute the voi change resulting from merging candidate
+            - vi-sign: compute the vi change resulting from merging candidate
             regions. Use the sign of the change as the training label.
             - rand-sign: compute the rand change resulting from merging the
             candidate regions. Use the sign of the change as the training
             label.
         """
-        label_type_keys = {'assignment':0, 'voi-sign':1, 'rand-sign':2, 
+        label_type_keys = {'assignment':0, 'vi-sign':1, 'rand-sign':2, 
                                                                 'boundary':3}
         assignments = [(ct == ct.max(axis=1)[:,newaxis]) for ct in ctables]
         g = self
@@ -827,13 +827,13 @@ class Rag(Graph):
         ar[ids] = ls.astype(ar.dtype)
         return ar.reshape(self.watershed.shape)
 
-    def split_voi(self, gt=None):
+    def split_vi(self, gt=None):
         if self.gt is None and gt is None:
             return array([0,0])
         elif self.gt is not None:
-            return split_voi(None, None, self.rig)
+            return split_vi(None, None, self.rig)
         else:
-            return split_voi(self.get_segmentation(), gt, None, [0], [0])
+            return split_vi(self.get_segmentation(), gt, None, [0], [0])
 
     def write(self, fout, format='GraphML'):
         pass
@@ -946,26 +946,26 @@ def ordered_priority(edges):
         return d.get((n1,n2), inf)
     return ord
 
-def expected_change_voi(feature_extractor, classifier, alpha=1.0, beta=1.0):
+def expected_change_vi(feature_extractor, classifier, alpha=1.0, beta=1.0):
     prob_func = classifier_probability(feature_extractor, classifier)
     def predict(g, n1, n2):
         p = prob_func(g, n1, n2) # Prediction from the classifier
-        # Calculate change in VOI if n1 and n2 should not be merged
-        v = compute_local_voi_change(
+        # Calculate change in VI if n1 and n2 should not be merged
+        v = compute_local_vi_change(
             len(g.node[n1]['extent']), len(g.node[n2]['extent']), g.volume_size
         )
         # Return expected change
         return  (p*alpha*v + (1.0-p)*(-beta*v))
     return predict
 
-def compute_local_voi_change(s1, s2, n):
-    """Compute change in VOI if we merge disjoint sizes s1,s2 in a volume n."""
+def compute_local_vi_change(s1, s2, n):
+    """Compute change in VI if we merge disjoint sizes s1,s2 in a volume n."""
     py1 = float(s1)/n
     py2 = float(s2)/n
     py = py1+py2
     return -(py1*log2(py1) + py2*log2(py2) - py*log2(py))
     
-def compute_true_delta_voi(ctable, n1, n2):
+def compute_true_delta_vi(ctable, n1, n2):
     p1 = ctable[n1].sum()
     p2 = ctable[n2].sum()
     p3 = p1+p2
