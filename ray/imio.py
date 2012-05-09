c3952bcef61b29881a5a2b55ecfb7741adb79485
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-05-09 17:55:43 -0400

    Clean up imio.py

--- ray/imio.py
+++ ray/imio.py
@@ -21,29 +21,12 @@ from numpy import array, asarray, uint8, uint16, uint32, uint64, zeros, \
     zeros_like, squeeze, fromstring, ndim, concatenate, newaxis, swapaxes, \
     savetxt, unique, double, ones, ones_like, prod, cumsum, ndarray
 import numpy as np
-try:
-    from numpy import imread
-except ImportError:
-    try:
-        from matplotlib.pyplot import imread
-    except:
-        logging.warning('unable to load numpy.imread or matplotlib.imread')
-        def imread(*args, **kwargs):
-            raise RuntimeError('Function imread not imported.')
 
 # local files
 import evaluate
 import morpho
 
-arguments = argparse.ArgumentParser(add_help=False)
-arggroup = arguments.add_argument_group('Image IO options')
-arggroup.add_argument('-I', '--invert-image', action='store_true',
-    default=False,
-    help='Invert the probabilities before segmenting.'
-)
-arggroup.add_argument('-m', '--median-filter', action='store_true', 
-    default=False, help='Run a median filter on the input image.'
-)
+### Random utilities
 
 def tryint(s):
     try:
@@ -61,6 +44,8 @@ def alphanumeric_key(s):
     on 2011-09-01
     """
     return [tryint(c) for c in re.split('([0-9]+)', s)]
+
+### Auto-detect file format
 
 supported_image_extensions = ['png', 'tif', 'tiff', 'jpg', 'jpeg']
 
@@ -129,6 +114,20 @@ def single_arg_read_image_stack(fn):
         print err
         raise
 
+def write_image_stack(npy_vol, fn, **kwargs):
+    """Write a numpy.ndarray 3D volume to a stack of images or an HDF5 file."""
+    fn = os.path.expanduser(fn)
+    if fn.endswith('.png'):
+        write_png_image_stack(npy_vol, fn, **kwargs)
+    elif fn.endswith('.h5'):
+        write_h5_stack(npy_vol, fn, **kwargs)
+    elif fn.endswith('.vtk'):
+        write_vtk(npy_vol, fn, **kwargs)
+    else:
+        raise ValueError('Image format not supported: ' + fn + '\n')
+
+### Standard image formats (png, tiff, etc.)
+
 def pil_to_numpy(img):
     return squeeze(array(img.getdata()).reshape((img.size[1], img.size[0], -1)))
 
@@ -147,41 +146,39 @@ def read_multi_page_tif(fn, crop=[None]*6):
         except EOFError:
             eof = True
     return concatenate(pages, axis=-1)
-        
 
-shiv_typecode_to_numpy_type = {
-    0:np.int8, 1:np.uint8, 2:np.int16, 3:np.uint16,
-    4:np.int32, 5:np.uint32, 6:np.int64, 7:np.uint64,
-    8:np.float32, 9:np.float64
-}
+def write_png_image_stack(npy_vol, fn, **kwargs):
+    """Write a numpy.ndarray 3D volume to a stack of .png images.
 
-def read_shiv_raw_stack(ws_fn, sp2body_fn):
-    ws_fn, sp2body_fn = map(os.path.expanduser, [ws_fn, sp2body_fn])
-    ws = read_shiv_raw_array(ws_fn)
-    sp2b = read_shiv_raw_array(sp2body_fn)[1]
-    ar = sp2b[ws]
-    return remove_merged_boundaries(ar)
+    Only 8-bit and 16-bit single-channel images are currently supported.
+    """
+    axis = kwargs.get('axis', -1)
+    bitdepth = kwargs.get('bitdepth', None)
+    npy_vol = swapaxes(npy_vol, 0, axis)
+    fn = os.path.expanduser(fn)
+    if 0 <= npy_vol.max() <= 1 and npy_vol.dtype == double:
+        bitdepth = 16 if None else bitdepth
+        imdtype = uint16 if bitdepth == 16 else uint8
+        npy_vol = ((2**bitdepth-1)*npy_vol).astype(imdtype)
+    if 1 < npy_vol.max() < 256 and bitdepth == None or bitdepth == 8:
+        mode = 'L'
+        mode_base = 'L'
+        npy_vol = uint8(npy_vol)
+    elif 256 <= numpy.max(npy_vol) < 2**16 and bitdepth == None or \
+                                                bitdepth == 16:
+        mode = 'I;16'
+        mode_base = 'I'
+        npy_vol = uint16(npy_vol)
+    else:
+        mode = 'RGBA'
+        mode_base = 'RGBA'
+        npy_vol = uint32(npy_vol)
+    for z, pl in enumerate(npy_vol):
+        im = Image.new(mode_base, pl.T.shape)
+        im.fromstring(pl.tostring(), 'raw', mode)
+        im.save(fn % z)
 
-def remove_merged_boundaries(ar):
-    import morpho
-    arp = morpho.pad(ar, [0,ar.max()+1])
-    arpr = arp.ravel()
-    zero_idxs = (arpr == 0).nonzero()[0]
-    ns = arpr[morpho.get_neighbor_idxs(arp, zero_idxs)]
-    ns_compl = ns.copy()
-    ns_compl[ns==0] = ns.max()+1
-    merged_boundaries = (ns.max(axis=1) == ns_compl.min(axis=1)).nonzero()[0]
-    arpr[zero_idxs[merged_boundaries]] = ns.max(axis=1)[merged_boundaries]
-    return morpho.juicy_center(arp, 2)
-
-def read_shiv_raw_array(fn):
-    fin = open(fn, 'rb')
-    typecode = fromstring(fin.read(4), uint8)[1]
-    ar_type = shiv_typecode_to_numpy_type[typecode]
-    ar_ndim = fromstring(fin.read(4), uint8)[0]
-    ar_shape = fromstring(fin.read(ar_ndim*4), uint32)
-    ar = fromstring(fin.read(), ar_type).reshape(ar_shape, order='F')
-    return ar
+### VTK structured points array format
 
 numpy_type_to_vtk_string = {
     np.uint8:'unsigned_char', np.int8:'char', np.uint16:'unsigned_short',
@@ -236,6 +233,8 @@ def read_vtk(fin, **kwargs):
     ar = squeeze(fromstring(f.read(), ar_type).reshape(ar_shape+[-1]))
     return ar
 
+### HDF5 format
+
 def read_h5_stack(fn, *args, **kwargs):
     """Read a volume in HDF5 format into numpy.ndarray.
 
@@ -264,6 +263,32 @@ def read_h5_stack(fn, *args, **kwargs):
     ar = array(a)
     dset.close()
     return ar
+
+def write_h5_stack(npy_vol, fn, **kwargs):
+    """Write a numpy.ndarray 3D volume to an HDF5 file.
+
+    The following keyword arguments are supported:
+    - 'group': the group into which to write the array. (default: 'stack')
+    - 'compression': The type of compression. (default: None)
+    - 'chunks': Chunk size in the HDF5 file. (default: None)
+    """
+    fn = os.path.expanduser(fn)
+    if not kwargs.has_key('compression'):
+        kwargs['compression'] = None
+    if not kwargs.has_key('chunks'):
+        kwargs['chunks'] = None
+    try:
+        group = kwargs['group']
+        del kwargs['group']
+    except KeyError:
+        group = 'stack'
+    fout = h5py.File(fn, 'a')
+    if group in fout:
+        del fout[group]
+    fout.create_dataset(group, data=npy_vol, **kwargs)
+    fout.close()
+
+### Raveler format
 
 def ucm_to_raveler(ucm, sp_threshold=0, body_threshold=0.1, **kwargs):
     """Return Raveler map from a UCM."""
@@ -392,7 +417,6 @@ def write_to_raveler(sps, sp_to_segment, segment_to_body, directory, gray=None,
     # make permissions friendly for proofreaders.
     subprocess.call(['chmod', '-R', 'go=u', directory])
 
-
 def raveler_output_shortcut(svs, seg, gray, outdir, sps_out=None):
     """Compute the Raveler format and write to directory, all at once."""
     sps_out, sp2seg, seg2body = segs_to_raveler(svs, seg, sps_out=sps_out)
@@ -457,73 +481,7 @@ def raveler_to_labeled_volume(rav_export_dir, get_glia=False,
     else:
         return output_volume
 
-def write_image_stack(npy_vol, fn, **kwargs):
-    """Write a numpy.ndarray 3D volume to a stack of images or an HDF5 file."""
-    fn = os.path.expanduser(fn)
-    if fn.endswith('.png'):
-        write_png_image_stack(npy_vol, fn, **kwargs)
-    elif fn.endswith('.h5'):
-        write_h5_stack(npy_vol, fn, **kwargs)
-    elif fn.endswith('.npy'):
-        write_npy_image_stack(npy_vol, fn, **kwargs)
-    else:
-        raise ValueError('Image format not supported: ' + fn + '\n')
-
-def write_png_image_stack(npy_vol, fn, **kwargs):
-    """Write a numpy.ndarray 3D volume to a stack of .png images.
-
-    Only 8-bit and 16-bit single-channel images are currently supported.
-    """
-    import morpho
-    axis = kwargs.get('axis', -1)
-    bitdepth = kwargs.get('bitdepth', None)
-    npy_vol = swapaxes(npy_vol, 0, axis)
-    fn = os.path.expanduser(fn)
-    if 0 <= npy_vol.max() <= 1 and npy_vol.dtype == double:
-        bitdepth = 16 if None else bitdepth
-        imdtype = uint16 if bitdepth == 16 else uint8
-        npy_vol = ((2**bitdepth-1)*npy_vol).astype(imdtype)
-    if 1 < npy_vol.max() < 256 and bitdepth == None or bitdepth == 8:
-        mode = 'L'
-        mode_base = 'L'
-        npy_vol = uint8(npy_vol)
-    elif 256 <= numpy.max(npy_vol) < 2**16 and bitdepth == None or \
-                                                bitdepth == 16:
-        mode = 'I;16'
-        mode_base = 'I'
-        npy_vol = uint16(npy_vol)
-    else:
-        mode = 'RGBA'
-        mode_base = 'RGBA'
-        npy_vol = uint32(npy_vol)
-    for z, pl in enumerate(npy_vol):
-        im = Image.new(mode_base, pl.T.shape)
-        im.fromstring(pl.tostring(), 'raw', mode)
-        im.save(fn % z)
-
-def write_h5_stack(npy_vol, fn, **kwargs):
-    """Write a numpy.ndarray 3D volume to an HDF5 file.
-
-    The following keyword arguments are supported:
-    - 'group': the group into which to write the array. (default: 'stack')
-    - 'compression': The type of compression. (default: None)
-    - 'chunks': Chunk size in the HDF5 file. (default: None)
-    """
-    fn = os.path.expanduser(fn)
-    if not kwargs.has_key('compression'):
-        kwargs['compression'] = None
-    if not kwargs.has_key('chunks'):
-        kwargs['chunks'] = None
-    try:
-        group = kwargs['group']
-        del kwargs['group']
-    except KeyError:
-        group = 'stack'
-    fout = h5py.File(fn, 'a')
-    if group in fout:
-        del fout[group]
-    fout.create_dataset(group, data=npy_vol, **kwargs)
-    fout.close()
+### Ilastik formats
 
 # obtained from Ilastik 0.5.4
 ilastik_label_colors = \
@@ -586,3 +544,39 @@ def read_prediction_from_ilastik_batch(fn, **kwargs):
     if kwargs.get('single_channel', True):
         a = a[...,0]
     return a
+
+### Shiv Vitaladevuni's binary raw array format
+
+shiv_typecode_to_numpy_type = {
+    0:np.int8, 1:np.uint8, 2:np.int16, 3:np.uint16,
+    4:np.int32, 5:np.uint32, 6:np.int64, 7:np.uint64,
+    8:np.float32, 9:np.float64
+}
+
+def read_shiv_raw_stack(ws_fn, sp2body_fn):
+    ws_fn, sp2body_fn = map(os.path.expanduser, [ws_fn, sp2body_fn])
+    ws = read_shiv_raw_array(ws_fn)
+    sp2b = read_shiv_raw_array(sp2body_fn)[1]
+    ar = sp2b[ws]
+    return remove_merged_boundaries(ar)
+
+def remove_merged_boundaries(ar):
+    import morpho
+    arp = morpho.pad(ar, [0,ar.max()+1])
+    arpr = arp.ravel()
+    zero_idxs = (arpr == 0).nonzero()[0]
+    ns = arpr[morpho.get_neighbor_idxs(arp, zero_idxs)]
+    ns_compl = ns.copy()
+    ns_compl[ns==0] = ns.max()+1
+    merged_boundaries = (ns.max(axis=1) == ns_compl.min(axis=1)).nonzero()[0]
+    arpr[zero_idxs[merged_boundaries]] = ns.max(axis=1)[merged_boundaries]
+    return morpho.juicy_center(arp, 2)
+
+def read_shiv_raw_array(fn):
+    fin = open(fn, 'rb')
+    typecode = fromstring(fin.read(4), uint8)[1]
+    ar_type = shiv_typecode_to_numpy_type[typecode]
+    ar_ndim = fromstring(fin.read(4), uint8)[0]
+    ar_shape = fromstring(fin.read(ar_ndim*4), uint32)
+    ar = fromstring(fin.read(), ar_type).reshape(ar_shape, order='F')
+    return ar
