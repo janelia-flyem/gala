d6ed3a8760d214ba8a68dbad1007a9c86b94fd06
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-05-11 17:40:46 -0400

    Replace numpy calls with np
    Initial synaptic_vi function generator.

--- ray/evaluate.py
+++ ray/evaluate.py
@@ -1,4 +1,4 @@
-import numpy
+import np as np
 import multiprocessing
 import itertools as it
 import h5py
@@ -8,8 +8,8 @@ from scipy.spatial.distance import pdist, cdist, squareform
 from scipy.misc import comb as nchoosek
 
 def pixel_wise_boundary_precision_recall(aseg, gt):
-    gt = (1-gt.astype(numpy.bool)).astype(numpy.uint8)
-    aseg = (1-aseg.astype(numpy.bool)).astype(numpy.uint8)
+    gt = (1-gt.astype(np.bool)).astype(np.uint8)
+    aseg = (1-aseg.astype(np.bool)).astype(np.uint8)
     tp = float((gt * aseg).sum())
     fp = (aseg * (1-gt)).sum()
     fn = (gt * (1-aseg)).sum()
@@ -24,7 +24,7 @@ def edit_distance(aseg, gt, ws=None):
 def edit_distance_to_bps(aseg, bps):
     aseg = relabel_from_one(aseg)[0]
     bps = relabel_from_one(bps)[0]
-    r = contingency_table(aseg, bps).astype(numpy.bool)
+    r = contingency_table(aseg, bps).astype(np.bool)
     if (bps==0).any(): r[:,0] = 0
     if (aseg==0).any(): r[0,:] = 0
     false_splits = (r.sum(axis=0)-1)[1:].sum()
@@ -32,13 +32,13 @@ def edit_distance_to_bps(aseg, bps):
     return (false_merges, false_splits)
 
 def relabel_from_one(a):
-    labels = numpy.unique(a)
+    labels = np.unique(a)
     labels0 = labels[labels!=0]
     m = labels.max()
     if m == len(labels0): # nothing to do, already 1...n labels
         return a, labels, labels
-    forward_map = numpy.zeros(m+1, int)
-    forward_map[labels0] = numpy.arange(1, len(labels0)+1)
+    forward_map = np.zeros(m+1, int)
+    forward_map[labels0] = np.arange(1, len(labels0)+1)
     inverse_map = labels
     return forward_map[a], forward_map, inverse_map
 
@@ -46,10 +46,10 @@ def contingency_table(seg, gt, ignore_seg=[0], ignore_gt=[0], norm=True):
     """Return the contingency table for all regions in matched segmentations."""
     gtr = gt.ravel()
     segr = seg.ravel() 
-    ij = numpy.zeros((2,len(gtr)))
+    ij = np.zeros((2,len(gtr)))
     ij[0,:] = segr
     ij[1,:] = gtr
-    cont = coo_matrix((numpy.ones((len(gtr))), ij)).toarray()
+    cont = coo_matrix((np.ones((len(gtr))), ij)).toarray()
     cont[:, ignore_gt] = 0
     cont[ignore_seg,:] = 0
     if norm:
@@ -63,12 +63,30 @@ def xlogx(x, out=None):
         y = x.copy()
     else:
         y = out
-    y[nz] *= numpy.log2(y[nz])
+    y[nz] *= np.log2(y[nz])
     return y
 
-def vi(x, y=None, weights=numpy.ones(2), ignore_x=[0], ignore_y=[0]):
+def special_points_evaluate(eval_fct, coords, flatten=True, coord_format=True):
+    def special_eval_fct(x, y, *args, **kwargs):
+        sx = np.zeros_like(x)
+        sy = np.zeros_like(y)
+        if coord_format:
+            coords = tuple([coords[:,i] for i in range(coords.shape[1])])
+        if flatten:
+            coords = np.ravel_multi_index(coords, x.shape)
+        sx.ravel() = x.ravel()[coords]
+        sy.ravel() = y.ravel()[coords]
+        return eval_fct(sx, sy, *args, **kwargs)
+
+def make_synaptic_vi(fn):
+    synapse_coords = \
+        syngeo.io.raveler_synapse_annotations_to_coords(fn, 'arrays')
+    synapse_coords = np.array(list(it.chain(*synapse_coords)))
+    return special_points_evaluate(split_vi, synapse_coords)
+
+def vi(x, y=None, weights=np.ones(2), ignore_x=[0], ignore_y=[0]):
     """Return the variation of information metric."""
-    return numpy.dot(weights, split_vi(x, y, ignore_x, ignore_y))
+    return np.dot(weights, split_vi(x, y, ignore_x, ignore_y))
 
 def split_vi(x, y=None, ignore_x=[0], ignore_y=[0]):
     """Return the symmetric conditional entropies associated with the VI.
@@ -83,7 +101,7 @@ def split_vi(x, y=None, ignore_x=[0], ignore_y=[0]):
     """
     _, _, _ , hxgy, hygx, _, _ = vi_tables(x, y, ignore_x, ignore_y)
     # false merges, false splits
-    return numpy.array([hygx.sum(), hxgy.sum()])
+    return np.array([hygx.sum(), hxgy.sum()])
 
 def vi_pairwise_matrix(segs, split=False):
     """Compute the pairwise VI distances within a set of segmentations.
@@ -93,13 +111,13 @@ def vi_pairwise_matrix(segs, split=False):
 
     0-labeled pixels are ignored.
     """
-    d = numpy.array([s.ravel() for s in segs])
+    d = np.array([s.ravel() for s in segs])
     if split:
         def dmerge(x, y): return split_vi(x, y)[0]
         def dsplit(x, y): return split_vi(x, y)[1]
         merges, splits = [squareform(pdist(d, df)) for df in [dmerge, dsplit]]
         out = merges
-        tri = numpy.tril(numpy.ones(splits.shape), -1).astype(bool)
+        tri = np.tril(np.ones(splits.shape), -1).astype(bool)
         out[tri] = splits[tri]
     else:
         out = squareform(pdist(d, vi))
@@ -122,11 +140,11 @@ def split_vi_threshold(tup):
 
 def vi_by_threshold(ucm, gt, ignore_seg=[], ignore_gt=[], npoints=None,
                                                             nprocessors=None):
-    ts = numpy.unique(ucm)[1:]
+    ts = np.unique(ucm)[1:]
     if npoints is None:
         npoints = len(ts)
     if len(ts) > 2*npoints:
-        ts = ts[numpy.arange(1, len(ts), len(ts)/npoints)]
+        ts = ts[np.arange(1, len(ts), len(ts)/npoints)]
     if nprocessors == 1: # this should avoid pickling overhead
         result = [split_vi_threshold((ucm, gt, ignore_seg, ignore_gt, t))
                 for t in ts]
@@ -134,22 +152,20 @@ def vi_by_threshold(ucm, gt, ignore_seg=[], ignore_gt=[], npoints=None,
         p = multiprocessing.Pool(nprocessors)
         result = p.map(split_vi_threshold, 
             ((ucm, gt, ignore_seg, ignore_gt, t) for t in ts))
-    return numpy.concatenate(
-                            (ts[numpy.newaxis, :], numpy.array(result).T), 
-                            axis=0)
+    return np.concatenate((ts[np.newaxis, :], np.array(result).T), axis=0)
 
 def rand_by_threshold(ucm, gt, npoints=None):
-    ts = numpy.unique(ucm)[1:]
+    ts = np.unique(ucm)[1:]
     if npoints is None:
         npoints = len(ts)
     if len(ts) > 2*npoints:
-        ts = ts[numpy.arange(1, len(ts), len(ts)/npoints)]
-    result = numpy.zeros((2,len(ts)))
+        ts = ts[np.arange(1, len(ts), len(ts)/npoints)]
+    result = np.zeros((2,len(ts)))
     for i, t in enumerate(ts):
         seg = label(ucm<t)[0]
         result[0,i] = rand_index(seg, gt)
         result[1,i] = adj_rand_index(seg, gt)
-    return numpy.concatenate((ts[numpy.newaxis, :], result), axis=0)
+    return np.concatenate((ts[np.newaxis, :], result), axis=0)
 
 def vi_tables(x, y=None, ignore_x=[0], ignore_y=[0]):
     """Return probability tables used for calculating VI.
@@ -175,13 +191,13 @@ def vi_tables(x, y=None, ignore_x=[0], ignore_y=[0]):
     nzpxy = pxy[nzx,:][:,nzy]
 
     # Calculate log conditional probabilities and entropies
-    ax = numpy.newaxis
-    lpygx = numpy.zeros(numpy.shape(px))
+    ax = np.newaxis
+    lpygx = np.zeros(np.shape(px))
     lpygx[nzx] = xlogx(nzpxy / nzpx[:,ax]).sum(axis=1) 
                         # \sum_x{p_{y|x} \log{p_{y|x}}}
     hygx = -(px*lpygx) # \sum_x{p_x H(Y|X=x)} = H(Y|X)
     
-    lpxgy = numpy.zeros(numpy.shape(py))
+    lpxgy = np.zeros(np.shape(py))
     lpxgy[nzy] = xlogx(nzpxy / nzpy[ax,:]).sum(axis=0)
     hxgy = -(py*lpxgy)
 
@@ -220,8 +236,8 @@ def split_components(idx, contingency, num_elems=4, axis=0):
     """
     if axis == 1:
         contingency = contingency.T
-    cc = contingency / contingency.sum(axis=1)[:,numpy.newaxis]
-    cct = contingency / contingency.sum(axis=0)[numpy.newaxis,:]
+    cc = contingency / contingency.sum(axis=1)[:,np.newaxis]
+    cct = contingency / contingency.sum(axis=0)[np.newaxis,:]
     idxs = (-cc[idx]).argsort()[:num_elems]
     probs = cc[idx][idxs]
     probst = cct[idx][idxs]
@@ -257,16 +273,16 @@ def fm_index(x, y=None):
     """ Return the Fowlkes-Mallows index. """
     cont = x if y is None else contingency_table(x, y, norm=False)
     a, b, c, d = rand_values(cont)
-    return a/(numpy.sqrt((a+b)*(a+c)))
+    return a/(np.sqrt((a+b)*(a+c)))
 
 def reduce_vi(fn='testing/%i/flat-single-channel-tr%i-%i-%.2f.lzf.h5',
     iterable=[(ts, tr, ts) for ts, tr in it.combinations(range(8), 2)],
-    thresholds=numpy.arange(0, 1.01, 0.01)):
+    thresholds=np.arange(0, 1.01, 0.01)):
     iterable = list(iterable)
-    vi = numpy.zeros((3, len(thresholds), len(iterable)), numpy.double)
+    vi = np.zeros((3, len(thresholds), len(iterable)), np.double)
     for i, t in enumerate(thresholds):
         for j, v in enumerate(iterable):
             f = h5py.File(fn % (tuple(v) + (t,)), 'r')
-            vi[:, i, j] += numpy.array(f['vi'])[:, 0]
+            vi[:, i, j] += np.array(f['vi'])[:, 0]
             f.close()
     return vi
