8e814b1bfb4a79abb5989f92899421d2f5054a23
Steve Plaza <plazas@janelia.hhmi.org>
2014-04-10 13:39:16 -0400

    writes max id from label volume

--- gala/watershed.py
+++ gala/watershed.py
@@ -196,6 +196,9 @@ def gen_watershed(session_location, options, master_logger, image_filename=None)
     imio.write_image_stack(labels,
         session_location + "/" + "supervoxels.h5")
 
+    fout = open(session_location + "/max_body.json", 'w')
+    fout.write(json.dumps({ "max_id": labels.max() }, indent=4))
+
 def entrypoint(argv):
     applogger = app_logger.AppLogger(False, 'gen-watershed')
     master_logger = applogger.get_logger()
