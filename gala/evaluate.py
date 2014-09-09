65500a419946c32d3669799739c5d0bd8608a2b9
Neal J. Donnelly <NealJMD@gmail.com>
2014-09-09 19:00:20 -0400

    added adjusted Rand error metric to evaluate.py

--- gala/evaluate.py
+++ gala/evaluate.py
@@ -710,6 +710,62 @@ def rand_by_threshold(ucm, gt, npoints=None):
         result[1, i] = adj_rand_index(seg, gt)
     return np.concatenate((ts[np.newaxis, :], result), axis=0)
 
+def rand_error(seg, gt, all_stats=False):
+    """Compute Adapted Rand error as defined by the SNEMI3D contest as
+    = 1 - the maximal F-score of the Rand index (excluding the zero 
+    component of the original labels). Adapted from the SNEMI3D MATLAB script,
+    hence the strange style.
+
+    Parameters
+    ----------
+    seg : np.ndarray
+        the segmentation to score, where each value is the label at that point
+    gt : np.ndarray, same shape as gt
+        the groundtruth to score against, where each value is a label
+    all_stats : boolean, default is False
+        whether to also return precision and recall as a 3-tuple with rand_error
+
+    Returns
+    -------
+    re : float
+        Rand error
+    OR
+    (re, precision, recall) : tuple of floats
+        Rand error, precision, and recall
+    """
+    # segA is truth, segB is query
+    segA = np.ravel(gt)
+    segB = np.ravel(seg)
+    n = segA.size
+
+    n_labels_A = np.amax(segA) + 1
+    n_labels_B = np.amax(segB) + 1
+
+    ones_data = np.ones(n)
+
+    p_ij = sparse.csr_matrix((ones_data, (segA[:], segB[:])), shape=(n_labels_A, n_labels_B))
+
+    a = p_ij[1:n_labels_A,:]
+    b = p_ij[1:n_labels_A,1:n_labels_B]
+    c = p_ij[1:n_labels_A,0].todense()
+    d = np.array(b.todense()) ** 2
+
+    a_i = np.array(a.sum(1))
+    b_i = np.array(b.sum(0))
+
+    sumA = np.sum(a_i * a_i)
+    sumB = np.sum(b_i * b_i) + (np.sum(c) / n)
+    sumAB = np.sum(d) + (np.sum(c) / n)
+
+    precision = sumAB / sumB
+    recall = sumAB / sumA
+
+    fScore = 2.0 * precision * recall / (precision + recall)
+    re = 1.0 - fScore
+    
+    if all_stats: return (precision, recall, re)
+    else: return re
+
 
 def calc_entropy(split_vals, count):
     col_count = 0
