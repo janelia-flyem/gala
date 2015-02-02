302b5a834926d5f4cdbd6f7d33b5ed70d19e386b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-02-03 00:25:40 +1100

    Update gala tests to use naive bayes

--- tests/test_gala.py
+++ tests/test_gala.py
@@ -6,12 +6,13 @@ from contextlib import contextmanager
 
 PYTHON_VERSION = sys.version_info[0]
 
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 import numpy as np
 from sklearn.externals import joblib
+from sklearn.naive_bayes import GaussianNB
 import subprocess as sp
 from gala import imio, classify, features, agglo, evaluate as ev
-from six.moves import map
+from six.moves import map, cPickle as pickle
 
 
 @contextmanager
@@ -44,6 +45,15 @@ fc = features.base.Composite(children=[fm, fh])
 
 ### helper functions
 
+
+def load_pickle(fn):
+    with open(fn, 'rb') as fin:
+        if PYTHON_VERSION == 3:
+            return pickle.load(fin, encoding='bytes', fix_imports=True)
+        else:  # Python 2
+            return pickle.load(fin)
+
+
 def load_training_data(fn):
     io = np.load(fn)
     X, y = io['X'], io['y']
@@ -64,13 +74,32 @@ def train_and_save_classifier(training_data_file, filename,
 ### tests
 
 def test_generate_examples_1_channel():
+    """Run a flat epoch and an active epoch of learning, compare learned sets.
+
+    The *order* of the edges learned by learn_flat is not guaranteed, so we
+    test the *set* of learned edges for the flat epoch. The learned epoch
+    *should* have a fixed order, so we test array equality.
+
+    Uses 1 channel probabilities.
+    """
     g_train = agglo.Rag(ws_train, pr_train, feature_manager=fc)
-    np.random.RandomState(0)
-    (X, y, w, merges) = g_train.learn_agglomerate(gt_train, fc, random_state=0)[0]
-    X_expected, y_expected = load_training_data(
-            os.path.join(rundir, 'example-data/train-set-1.npz'))
-    assert_allclose(X, X_expected, atol=1e-6)
-    assert_allclose(y, y_expected, atol=1e-6)
+    _, alldata = g_train.learn_agglomerate(gt_train, fc,
+                                           classifier='naive bayes')
+    testfn = ('example-data/train-naive-bayes-merges1-py3.pck'
+              if PYTHON_VERSION == 3 else
+              'example-data/train-naive-bayes-merges1-py2.pck')
+    exp0, exp1 = load_pickle(os.path.join(rundir, testfn))
+    expected_edges = set(map(tuple, exp0))
+    edges = set(map(tuple, alldata[0][3]))
+    merges = alldata[1][3]
+    assert edges == expected_edges
+    assert_array_equal(merges, exp1)
+    nb = GaussianNB().fit(alldata[0][0], alldata[0][1][:, 0])
+    nbexp = joblib.load(os.path.join(rundir,
+                                     'example-data/naive-bayes-1.joblib'))
+    assert_allclose(nb.theta_, nbexp.theta_, atol=1e-10)
+    assert_allclose(nb.sigma_, nbexp.sigma_, atol=1e-4)
+    assert_allclose(nb.class_prior_, nbexp.class_prior_, atol=1e-7)
 
 
 def test_segment_with_classifer_1_channel():
@@ -92,13 +121,32 @@ def test_segment_with_classifer_1_channel():
 
 
 def test_generate_examples_4_channel():
+    """Run a flat epoch and an active epoch of learning, compare learned sets.
+
+    The *order* of the edges learned by learn_flat is not guaranteed, so we
+    test the *set* of learned edges for the flat epoch. The learned epoch
+    *should* have a fixed order, so we test array equality.
+
+    Uses 4 channel probabilities.
+    """
     g_train = agglo.Rag(ws_train, p4_train, feature_manager=fc)
-    np.random.RandomState(0)
-    (X, y, w, merges) = g_train.learn_agglomerate(gt_train, fc, random_state=0)[0]
-    X_expected, y_expected = load_training_data(
-            os.path.join(rundir, 'example-data/train-set-4.npz'))
-    assert_allclose(X, X_expected, atol=1e-6)
-    assert_allclose(y, y_expected, atol=1e-6)
+    _, alldata = g_train.learn_agglomerate(gt_train, fc,
+                                           classifier='naive bayes')
+    testfn = ('example-data/train-naive-bayes-merges4-py3.pck'
+              if PYTHON_VERSION == 3 else
+              'example-data/train-naive-bayes-merges4-py2.pck')
+    exp0, exp1 = load_pickle(os.path.join(rundir, testfn))
+    expected_edges = set(map(tuple, exp0))
+    edges = set(map(tuple, alldata[0][3]))
+    merges = alldata[1][3]
+    assert edges == expected_edges
+    assert_array_equal(merges, exp1)
+    nb = GaussianNB().fit(alldata[0][0], alldata[0][1][:, 0])
+    nbexp = joblib.load(os.path.join(rundir,
+                                     'example-data/naive-bayes-4.joblib'))
+    assert_allclose(nb.theta_, nbexp.theta_, atol=1e-10)
+    assert_allclose(nb.sigma_, nbexp.sigma_, atol=1e-4)
+    assert_allclose(nb.class_prior_, nbexp.class_prior_, atol=1e-7)
 
 
 def test_segment_with_classifier_4_channel():
