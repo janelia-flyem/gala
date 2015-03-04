9588196f4719c58b2f428a9e23cce3bacf5b5de1
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-03-04 18:44:54 +1100

    Update version to 0.2.1. Merely adds docs

--- gala/__init__.py
+++ gala/__init__.py
@@ -18,4 +18,4 @@ del sys, logging
 __all__ = ['agglo', 'morpho', 'evaluate', 'viz', 'imio', 'classify',
     'stack_np', 'app_logger', 'option_manager', 'features', 'filter']
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
