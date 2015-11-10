b06bf2a9c39da407188a69f38f5a611888431acc
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-11-10 22:03:32 +0930

    Optimize construction of label indices

--- gala/sparselol_cy.pyx
+++ gala/sparselol_cy.pyx
@@ -1,9 +1,16 @@
 cimport numpy as np
 from scipy import sparse
+import cython
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def extents_count(Py_ssize_t[::1] labels,
                   Py_ssize_t[::1] curr_loc, Py_ssize_t[::1] indices):
-    cdef Py_ssize_t i
-    for i, label in enumerate(labels):
+    cdef Py_ssize_t i = 0
+    cdef Py_ssize_t label
+    cdef Py_ssize_t size = len(labels)
+    for i in range(size):
+        label = labels[i]
         indices[curr_loc[label]] = i
         curr_loc[label] += 1
+        i += 1
