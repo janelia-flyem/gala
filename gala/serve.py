37227e4b8cf04b4ea869ed941856791c2ad3735b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-12-02 17:50:33 +1100

    First attempt at a proofreading simulator

--- gala/serve.py
+++ gala/serve.py
@@ -1,6 +1,7 @@
-from . import agglo, features, classify
+import numpy as np
+from sklearn.utils import check_random_state
 import zmq
-import json
+from . import agglo, features, classify, evaluate as ev
 
 
 _feature_manager = features.default.snemi3d()
@@ -38,7 +39,7 @@ class Solver(object):
         self.build_rag()
         self.comm = zmq.Context().socket(zmq.PAIR)
         self.addr = host + ':' + str(port)
-        self.comm.bind(host + ':' + str(port))
+        self.comm.bind(self.addr)
         self.history = []
         self.separate = []
         self.features = []
@@ -108,3 +109,58 @@ class Solver(object):
             self.rag.node[s0]['exclusions'].add(i)
             self.rag.node[s1]['exclusions'].add(i)
         self.rag.replay_merge_history(self.history)
+
+
+def proofread(fragments, true_segmentation, host='tcp://localhost', port=5556,
+              num_operations=10, mode='fast paint', random_state=None):
+    """Simulate a proofreader by sending and receiving messages to a Solver.
+
+    Parameters
+    ----------
+    fragments : array of int
+        The initial segmentation to be proofread.
+    true_segmentation : array of int
+        The target segmentation. Should be a superset of `fragments`.
+    host : string
+        The host to serve ZMQ commands to.
+    port : int
+        Port on which to connect ZMQ.
+    num_operations : int, optional
+        How many proofreading operations to perform before returning.
+    mode : string, optional
+        The mode with which to simulate proofreading.
+    random_state : None or int or numpy.RandomState instance, optional
+        Fix the random state for proofreading.
+
+    Returns
+    -------
+    lut : tuple of array-like of int
+        A look-up table from fragments (first array) to segments
+        (second array), obtained by requesting it from the Solver after
+        initial proofreading simulation.
+    """
+    true = agglo.best_possible_segmentation(fragments, true_segmentation)
+    base_graph = agglo.Rag(fragments)
+    comm = zmq.Context().socket(zmq.PAIR)
+    comm.connect(host + ':' + str(port))
+    ctable = ev.contingency_table(fragments, true).tocsc()
+    true_labels = np.unique(true)
+    random = check_random_state(random_state)
+    random.shuffle(true_labels)
+    for _, label in zip(range(num_operations), true_labels):
+        components = [int(i) for i in ctable.getcol(label).indices]
+        comm.send_json({'type': 'merge',
+                        'data': {'segments': components}})
+        for fragment in components:
+            for neighbor in base_graph[fragment]:
+                if neighbor not in components:
+                    edge = [fragment, neighbor]
+                    comm.send_json({'type': 'separate',
+                                    'data': {'segments': edge}})
+
+    comm.send_json({'type': 'request',
+                    'data': {'what': 'fragment-segment-lut'}})
+    response = comm.recv_json()
+    src = response['data']['fragments']
+    dst = response['data']['segments']
+    return src, dst
